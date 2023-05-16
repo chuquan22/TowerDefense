3b8b5d2c906d6a39c64e11a755014da337c3192a
chuquan22 <81926487+chuquan22@users.noreply.github.com>
2023-05-16 21:51:56 +0700

    update Tower

---
+++ Assets/Script/ITower.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Script
+{
+    interface ITower
+    {
+        public void Attack();
+
+        public void UpdateTower();
+
+    }
+}
---
+++ Assets/Script/Tower.cs
@@ -0,0 +1,22 @@
+using Assets.Script;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Tower : ITower
+{
+    public double attackRange { get; set; }
+    public double damage { get; set; }
+    public double price { get; set;  }
+    public void Attack()
+    {
+        
+    }
+
+    public void UpdateTower()
+    {
+        
+    }
+
+    
+}
