ae65da59205f6127fbb812ec270714e183eb6ce3
Nguyễn Thị Thu <74730786+nguyenthu120801@users.noreply.github.com>
2023-06-02 22:20:10 +0700

    thu

--- Assets/Script/Monster.cs
+++
@@ -1,9 +0,0 @@
-using Assets.Script;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Monster : IMonster
-{
-    
-}
---
+++ Assets/Script/MonsterSpawner.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+
+public class MonsterSpawner : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject[] MonsterPrefabs;
+
+    [Header("Attributes")]
+    [SerializeField] private int baseMonster = 8;
+    [SerializeField] private float MonsterPerSecond = 0.5f;
+    [SerializeField] private float timeBetweenWaves = 5f;
+    [SerializeField] private float difficultyScalingFactor = 0.75f;
+
+    [Header("Events")]
+    public static UnityEvent onMonsterDestroy = new UnityEvent();
+
+
+
+    private int currentWave = 1;
+    private float timeSinceLastSpawn;
+    private int monsterAlive;
+    private int MonsterLeftToSpawn;
+    private bool isSpawning = false;
+
+    private void Awake()
+    {
+        onMonsterDestroy.AddListener(MonsterDestroyed);
+    }
+    private void Start()
+    {
+        StartCoroutine(StartWave());
+    }
+
+
+    private void Update()
+    {
+        if (!isSpawning)
+        {
+            return;
+        }
+        timeSinceLastSpawn += Time.deltaTime;
+
+        if(timeSinceLastSpawn >= (1f/MonsterPerSecond) && MonsterLeftToSpawn > 0)
+        {
+            SpawnMonster();
+            MonsterLeftToSpawn--;
+            monsterAlive++;
+            timeSinceLastSpawn = 0f;
+        }
+
+        if(monsterAlive== 0 && MonsterLeftToSpawn ==0)
+        {
+            EndWave();
+        }
+    }
+
+    private void MonsterDestroyed()
+    {
+        monsterAlive--;
+    }
+    private IEnumerator StartWave()
+    {
+        yield return new WaitForSeconds(timeBetweenWaves);
+        isSpawning = true;
+        MonsterLeftToSpawn = MonsterPerWave();
+    }
+
+    private void EndWave()
+    {
+        isSpawning= false;
+        timeSinceLastSpawn = 0f;
+        currentWave++;
+        StartCoroutine(StartWave());
+    }
+
+    private void SpawnMonster()
+    {
+        Debug.Log("Spawn Monster");
+        GameObject prefabToWpawn = MonsterPrefabs[0];
+        Instantiate(prefabToWpawn, LevelManager.main.startPoint.position, Quaternion.identity);
+    }
+
+    private int MonsterPerWave()
+    {
+        return Mathf.RoundToInt(baseMonster * Mathf.Pow(currentWave, difficultyScalingFactor));
+    }
+}
