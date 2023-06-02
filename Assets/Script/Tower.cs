8a3c6c60569efe00e2678585f861936df071edc4
Duc8101 <vegitobasicinstict1810@gmail.com>
2023-06-02 22:35:46 +0700

    scene

--- Assets/Script/Tower.cs
+++ Assets/Script/Tower.cs
@@ -1,13 +1,13 @@
 using Assets.Script;
 using System.Collections;
 using System.Collections.Generic;
-
+using UnityEngine;
 
 public class Tower : ITower
 {
-    public double attackRange { get; set; }
-    public double damage { get; set; }
-    public double price { get; set;  }
+    public double AttackRange { get; set; }
+    public double Damage { get; set; }
+    public double Price { get; set;  }
     public void Attack()
     {
         
