16e96d1ca76008b7c6bf55efae9a89ebab47e292
Duc8101 <vegitobasicinstict1810@gmail.com>
2023-06-03 12:27:48 +0700

    scene

--- Assets/Script/GameOver.cs
+++ Assets/Script/GameOver.cs
@@ -1,13 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
+    public Button btnRestart;
+    public Button btnMenu;
+    public Button btnQuit;
     // Start is called before the first frame update
     void Start()
     {
-        
+        btnRestart.onClick.AddListener(Restart);
+        btnMenu.onClick.AddListener(Menu);
+        btnQuit.onClick.AddListener(Quit);
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    void Menu()
+    {
+        SceneManager.LoadScene("StartGame");
+    }
+
+    void Quit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
     }
 
     // Update is called once per frame
