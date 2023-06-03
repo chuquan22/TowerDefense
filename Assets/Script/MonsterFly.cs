7dc33e6364df5186b752b78e4dfbdb2adb85362f
chuquan22 <81926487+chuquan22@users.noreply.github.com>
2023-06-04 02:04:36 +0700

    Tower

---
+++ Assets/Script/MonsterFly.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterFly : Monster
+{
+    private void Start()
+    {
+        moveSpeed = 4f;
+    }
+}
