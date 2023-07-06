a9e5f10fa1ef2aeafbfd6f5b816de7a69980b7a2
Duc8101 <vegitobasicinstict1810@gmail.com>
2023-07-07 00:29:07 +0700

    update price

--- Assets/Script/MonsterFly.cs
+++ Assets/Script/MonsterFly.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class MonsterFly : Monster
 {
-
+    public static bool isMonsterFlyDestroyed = false;
+    public const int BONUS_PRICE_MONSTER_FLY = 150;
     public override void Start()
     {
         moveSpeed = 4f;
         base.Start();
     }
+
+    public override void TakeDamage(float damage)
+    {
+        currentHP -= damage;
+        Debug.Log(currentHP);
+        if (currentHP <= 0)
+        {
+            MonsterSpawner.onMonsterDestroy.Invoke();
+            Destroy(gameObject);
+            isMonsterFlyDestroyed = true;
+        }
+    }
+
+
 }
